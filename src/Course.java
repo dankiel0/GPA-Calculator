66598975c8d568904b28db75627611b2c58fdd42
Daniel Khrapko <55002392+khrapkodaniel@users.noreply.github.com>
2019-11-06 21:10:19 -0500

    Update Course.java

--- src/Course.java
+++ src/Course.java
@@ -8,8 +8,8 @@ public class Course {
 	
 	public String 	getName()			{return name;}
 	public Grade 	getGrade()			{return grade;}
-	public float 	getUnweightedGPA() 	{return unweightedGPA;}
-	public float 	getWeightedGPA()	{return weightedGPA;}
+	public float 	getUnweightedGPA() 		{return unweightedGPA;}
+	public float 	getWeightedGPA()		{return weightedGPA;}
 	public float 	getCredit()			{return credit / 5;}
 	public Type 	getType()			{return type;}
 	
@@ -44,7 +44,7 @@ public class Course {
 			case "D+": 	enumGrade = Grade.DPLUS; 	break;
 			case "D": 	enumGrade = Grade.D; 		break;
 			case "F":	enumGrade = Grade.F;		break;
-			default: 	enumGrade = null;			break;
+			default: 	enumGrade = null;		break;
 			}
 			
 			return enumGrade;
@@ -55,66 +55,66 @@ public class Course {
 			
 			// conversion scale for unweighted GPA.
 			switch(grade) {
-			case APLUS: case A: unweightedGPA = 4;		break;
+			case APLUS: case A: 	unweightedGPA = 4;	break;
 			case AMINUS: 		unweightedGPA = 3.7f;	break;
 			case BPLUS: 		unweightedGPA = 3.3f;	break;
-			case B: 			unweightedGPA =	3;		break;
+			case B: 		unweightedGPA =	3;	break;
 			case BMINUS: 		unweightedGPA = 2.7f;	break;
 			case CPLUS: 		unweightedGPA = 2.3f;	break;
-			case C: 			unweightedGPA = 2;		break;
+			case C: 		unweightedGPA = 2;	break;
 			case CMINUS: 		unweightedGPA = 1.7f;	break;
 			case DPLUS: 		unweightedGPA = 1.3f;	break;
-			case D: 			unweightedGPA = 1;		break;
-			default: 			unweightedGPA = 0;		break;
+			case D: 		unweightedGPA = 1;	break;
+			default: 		unweightedGPA = 0;	break;
 			}
 			
 			// conversion scales for weighted GPA.
 			if(type.equals(Type.Regular))
 				switch(grade) {
 				case APLUS: 	weightedGPA = 4.33f;	break;
-				case A: 		weightedGPA = 4;		break;
+				case A: 	weightedGPA = 4;	break;
 				case AMINUS: 	weightedGPA = 3.66f;	break;
 				case BPLUS: 	weightedGPA = 3.33f;	break;
-				case B: 		weightedGPA = 3;		break;
+				case B: 	weightedGPA = 3;	break;
 				case BMINUS: 	weightedGPA = 2.66f;	break;
 				case CPLUS: 	weightedGPA = 2.33f;	break;
-				case C: 		weightedGPA = 2;		break;
+				case C: 	weightedGPA = 2;	break;
 				case CMINUS: 	weightedGPA = 1.66f;	break;
 				case DPLUS: 	weightedGPA = 1.33f;	break;
-				case D: 		weightedGPA = 1;		break;
-				default: 		weightedGPA = 0;		break;
+				case D: 	weightedGPA = 1;	break;
+				default: 	weightedGPA = 0;	break;
 				}
 			
 			if(type.equals(Type.Honors))
 				switch(grade) {
 				case APLUS: 	weightedGPA = 5.08f;	break;
-				case A: 		weightedGPA = 4.75f;	break;
+				case A: 	weightedGPA = 4.75f;	break;
 				case AMINUS: 	weightedGPA = 4.41f;	break;
 				case BPLUS: 	weightedGPA = 4.08f;	break;
-				case B: 		weightedGPA = 3.75f;	break;
+				case B: 	weightedGPA = 3.75f;	break;
 				case BMINUS: 	weightedGPA = 3.41f;	break;
 				case CPLUS: 	weightedGPA = 3.08f;	break;
-				case C: 		weightedGPA = 2.75f;	break;
+				case C: 	weightedGPA = 2.75f;	break;
 				case CMINUS: 	weightedGPA = 2.41f;	break;
 				case DPLUS: 	weightedGPA = 2.08f;	break;
-				case D: 		weightedGPA = 1.75f;	break;
-				default: 		weightedGPA = 0;		break;
+				case D: 	weightedGPA = 1.75f;	break;
+				default: 	weightedGPA = 0;	break;
 				}
 			
 			if(type.equals(Type.AP))
 				switch(grade) {
 				case APLUS: 	weightedGPA = 5.83f;	break;
-				case A: 		weightedGPA = 5.5f;		break;
+				case A: 	weightedGPA = 5.5f;	break;
 				case AMINUS: 	weightedGPA = 5.16f;	break;
 				case BPLUS: 	weightedGPA = 4.83f;	break;
-				case B: 		weightedGPA = 4.5f;		break;
+				case B: 	weightedGPA = 4.5f;	break;
 				case BMINUS: 	weightedGPA = 4.16f;	break;
 				case CPLUS: 	weightedGPA = 3.83f;	break;
-				case C: 		weightedGPA = 3.5f;		break;
+				case C: 	weightedGPA = 3.5f;	break;
 				case CMINUS: 	weightedGPA = 3.16f;	break;
 				case DPLUS: 	weightedGPA = 2.83f;	break;
-				case D: 		weightedGPA = 2.5f;		break;
-				default: 		weightedGPA = 0;		break;
+				case D: 	weightedGPA = 2.5f;	break;
+				default: 	weightedGPA = 0;	break;
 				}
 			
 			return new Course.GPA(unweightedGPA, weightedGPA);
